336f1c7845c9600ec68a92b01a6ddb81c9dcb0ae
KimzaTech <kimzatech@gmail.com>
2022-08-26 19:28:05 +0300

    mian

---
+++ main.py
@@ -0,0 +1,15 @@
+# Initilize a python list
+python_list = [2, 3, 4, 5, 6, 7, 8, 9]
+
+# Display the list
+print(python_list)
+
+
+# Import numpy library
+import numpy as np
+
+# Converting the python list to numpy arrays
+numpy_array1 = np.array(python_list)
+
+# Display the numpy array
+print(numpy_array1)
